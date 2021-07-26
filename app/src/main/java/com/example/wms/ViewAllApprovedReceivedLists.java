2087f0adf6f8a2d0fc47e7e427ab89d458e03020
amirulezasyraf <85129638+amirulezasyraf@users.noreply.github.com>
2021-07-26 19:19:07 +0800

    the view approved received list can see but pp individual WIP

--- app/src/main/java/com/example/wms/ViewAllApprovedReceivedLists.java
+++ app/src/main/java/com/example/wms/ViewAllApprovedReceivedLists.java
@@ -10,23 +10,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.wms.adapters.ViewAllApprovedReceivedListsAdapter;
 import com.example.wms.adapters.ViewAllReceivingListAdapter;
 import com.example.wms.models.ApprovedReceivedList;
 import com.example.wms.models.ReceivingList;
 import com.example.wms.util.VerticalSpacingItemDecorator;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class  ViewAllApprovedReceivedLists extends AppCompatActivity implements ViewAllApprovedReceivedListsAdapter.OnApprovedReceivedListListener {
 
+    private String URL = "http://13.59.50.74/android_connect/viewapprovedreceivinglist.php";
     RecyclerView recyclerviewApprovedReceivedList;
     ViewAllApprovedReceivedListsAdapter viewAllApprovedReceivedListsAdapter;
     DrawerLayout drawerLayout;
+    ArrayList<ApprovedReceivedList> approvedreceivingList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +53,8 @@ public class  ViewAllApprovedReceivedLists extends AppCompatActivity implements 
         setSupportActionBar(toolbar);
 
         recyclerviewApprovedReceivedList = findViewById(R.id.recyclerviewApprovedReceivedList);
-        setRecyclerView();
+        approvedreceivingList = new ArrayList<ApprovedReceivedList>();
+        viewList();
     }
 
     //drawer settings
@@ -71,19 +86,53 @@ public class  ViewAllApprovedReceivedLists extends AppCompatActivity implements 
     }
 
     private void setRecyclerView(){
+        viewAllApprovedReceivedListsAdapter = new ViewAllApprovedReceivedListsAdapter(this,approvedreceivingList, this);
+        recyclerviewApprovedReceivedList.setAdapter(viewAllApprovedReceivedListsAdapter);
         recyclerviewApprovedReceivedList.setHasFixedSize(true);
         recyclerviewApprovedReceivedList.setLayoutManager(new LinearLayoutManager(this));
         VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
         recyclerviewApprovedReceivedList.addItemDecoration(itemDecorator);
-        viewAllApprovedReceivedListsAdapter = new ViewAllApprovedReceivedListsAdapter(this,getList(), this);
-        recyclerviewApprovedReceivedList.setAdapter(viewAllApprovedReceivedListsAdapter);
     }
 
-    private ArrayList<ApprovedReceivedList> getList(){
-        ArrayList <ApprovedReceivedList> approvedReceivedLists = new ArrayList<>();
-        approvedReceivedLists.add(new ApprovedReceivedList(1,87998, 84759, "ABC CO", "Not Received"));
-        return approvedReceivedLists;
+    private void viewList(){
+        Log.d("output", "loadproducts");
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray products = new JSONArray(response);
+
+                    for(int i = 0; i<products.length(); i++)
+                    {
+                        JSONObject productObject = products.getJSONObject(i);
+                        int sn = productObject.getInt("sn");
+                        int ponum = productObject.getInt("PONum");
+                        int donum = productObject.getInt("DONum");
+                        String supplier = productObject.getString("supplier");
+                        String date_rcv = productObject.getString("date_rcv");
+
+                        ApprovedReceivedList rl = new ApprovedReceivedList(sn, ponum, donum, supplier, date_rcv);
+                        approvedreceivingList.add(rl);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                setRecyclerView();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("output", "rb");
+                Toast.makeText(ViewAllApprovedReceivedLists.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        Log.d("output", stringRequest.toString());
+
+        Volley.newRequestQueue(this).add(stringRequest);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -111,7 +160,7 @@ public class  ViewAllApprovedReceivedLists extends AppCompatActivity implements 
         //Log.d (TAG, "onPPClick: clicked" + position);
 
         Intent intent = new Intent (this, IndividualApprovedReceivedList.class);
-        intent.putExtra("selectedApprovedReceivedList", getList().get(position));
+        intent.putExtra("selectedApprovedReceivedList", approvedreceivingList.get(position));
         startActivity(intent);
     }
 }
